46389f78a12259bb6fc5b626378fecacfa4f56a6
oscarmedina77 <oscar.medina77@thehutgroup.com>
2023-01-10 09:52:44 +0000

    completed steps 1-5 or readme

--- src/main/java/com/thg/accelerator23/connectn/ai/chatgptsucks/ConnectFork.java
+++ src/main/java/com/thg/accelerator23/connectn/ai/chatgptsucks/ConnectFork.java
@@ -1,14 +1,14 @@
-package com.thg.accelerator23.connectn.ai.yourteam;
+package com.thg.accelerator23.connectn.ai.chatgptsucks;
 
 import com.thehutgroup.accelerator.connectn.player.Board;
 import com.thehutgroup.accelerator.connectn.player.Counter;
 import com.thehutgroup.accelerator.connectn.player.Player;
 
 
-public class NameAi extends Player {
-  public NameAi(Counter counter) {
+public class ConnectFork extends Player {
+  public ConnectFork(Counter counter) {
     //TODO: fill in your name here
-    super(counter, NameAi.class.getName());
+    super(counter, ConnectFork.class.getName());
   }
 
   @Override
