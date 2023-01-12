8296e7fdfa87bd24540bc4e8d754edcf1d1ac27e
Robert Woodward-Massey <Robert.WoodwardMassey54@thehutgroup.com>
2023-01-12 17:27:47 +0000

    bored, pub time

--- src/main/java/com/thg/accelerator23/connectn/ai/chatgptsucks/ConnectForkMCTSv0.java
+++ src/main/java/com/thg/accelerator23/connectn/ai/chatgptsucks/ConnectForkMCTSv0.java
@@ -2,18 +2,18 @@ package com.thg.accelerator23.connectn.ai.chatgptsucks;
 
 import com.thehutgroup.accelerator.connectn.player.Board;
 import com.thehutgroup.accelerator.connectn.player.Counter;
-import com.thehutgroup.accelerator.connectn.player.InvalidMoveException;
 import com.thehutgroup.accelerator.connectn.player.Player;
 import com.thg.accelerator23.connectn.ai.chatgptsucks.analysis.BoardAnalyser;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
-
-
 
 public class ConnectForkMCTSv0 extends Player {
   private static Board board;
+
+//  Observation???
+//  global current_state?
 
   public ConnectForkMCTSv0(Counter counter) {
     //TODO: fill in your name here
@@ -22,8 +22,18 @@ public class ConnectForkMCTSv0 extends Player {
 
   @Override
   public int makeMove(Board board) {
+
+    LocalDateTime initTime = java.time.LocalDateTime.now();
+    double EMPTY = 0;
+    int T_max = 10;
+    double Cp_default = 1;
+
     BoardAnalyser boardAnalyser = new BoardAnalyser(board.getConfig());
     List<Integer> availableMoves = new ArrayList<>();
+
+
+
+//    DON'T FORGET CODE AFTER STATE CLASS
 
     return 3;
   }
