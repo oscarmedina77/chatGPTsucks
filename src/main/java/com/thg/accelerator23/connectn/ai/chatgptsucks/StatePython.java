8b822c6ac259c0e9f612f9f1385c1330bef65215
Robert Woodward-Massey <Robert.WoodwardMassey54@thehutgroup.com>
2023-01-12 16:24:57 +0000

    StatePython finished where we can, need to add other functions from earlier in Python script

--- src/main/java/com/thg/accelerator23/connectn/ai/chatgptsucks/StatePython.java
+++ src/main/java/com/thg/accelerator23/connectn/ai/chatgptsucks/StatePython.java
@@ -5,17 +5,74 @@ import com.thehutgroup.accelerator.connectn.player.GameConfig;
 import com.thehutgroup.accelerator.connectn.player.Position;
 import com.thg.accelerator23.connectn.ai.chatgptsucks.analysis.BoardAnalyser;
 
+import javax.lang.model.type.NullType;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Objects;
 import java.util.Random;
+
+import static java.util.Objects.isNull;
 
 public class StatePython {
     private Board board;
     private int mark;
     private GameConfig config;
-    private ArrayList<Objects> children;
-    private ArrayList<Objects> parent;
-    private int nodeTotalScore;
+    private ArrayList<StatePython> children;
+    private StatePython parent;
+    private double nodeTotalScore;
+
+    public Board getBoard() {
+        return board;
+    }
+
+    public int getMark() {
+        return mark;
+    }
+
+    public GameConfig getConfig() {
+        return config;
+    }
+
+    public ArrayList<StatePython> getChildren() {
+        return children;
+    }
+
+    public StatePython getParent() {
+        return parent;
+    }
+
+    public double getNodeTotalScore() {
+        return nodeTotalScore;
+    }
+
+    public int getNodeTotalVisits() {
+        return nodeTotalVisits;
+    }
+
+    public ArrayList<Integer> getAvailableMoves() {
+        return availableMoves;
+    }
+
+    public ArrayList<Integer> getExpandableMoves() {
+        return expandableMoves;
+    }
+
+    public boolean isTerminal() {
+        return isTerminal;
+    }
+
+    public int getTerminalScore() {
+        return terminalScore;
+    }
+
+    public int getActionTaken() {
+        return actionTaken;
+    }
+
+    public Position getPosition() {
+        return position;
+    }
+
     private int nodeTotalVisits;
     private ArrayList<Integer> availableMoves;
     private ArrayList<Integer> expandableMoves;
@@ -27,7 +84,7 @@ public class StatePython {
     public StatePython(Board board,
                        int mark,
                        GameConfig config,
-                       ArrayList<Objects> parent,
+                       StatePython parent,
                        boolean isTerminal,
                        int terminalScore,
                        int actionTaken,
@@ -39,7 +96,7 @@ public class StatePython {
         this.board = board;
         this.mark = mark;
         this.config = config;
-        this.children = new ArrayList<>();
+        this.children = new ArrayList<StatePython>();
         this.parent = parent;
         this.nodeTotalScore = 0;
         this.nodeTotalVisits = 0;
@@ -51,15 +108,110 @@ public class StatePython {
     }
 
     public boolean isExpandable() {
-        return !isTerminal && expandableMoves.size() > 0;
+        return !this.isTerminal && this.expandableMoves.size() > 0;
     }
 
     public void expandSimulateChild() {
         Random rand = new Random();
-        int randIndex = rand.nextInt(expandableMoves.size());
-        int column = expandableMoves.get(randIndex);
+        int randIndex = rand.nextInt(this.expandableMoves.size());
+        int column = this.expandableMoves.get(randIndex);
 
 //       board as copy again?
-        Board childBoard = board;
+        Board childBoard = this.board;
+
+//        play function here - will be in AI agent later
+
+//        check finish and score function here
+
+        StatePython currentState = new StatePython(board,
+                mark,
+                config,
+                parent,
+                isTerminal,
+                terminalScore,
+                actionTaken,
+                position);
+
+        StatePython newState1 = new StatePython(board,
+                mark,
+                config,
+                currentState,
+                isTerminal,
+                terminalScore,
+                actionTaken,
+                position);
+
+        StatePython newState2 = new StatePython(childBoard,
+//                mark needs to be opponentMark(mark) from a function added later
+                mark,
+                config,
+                newState1,
+                isTerminal,
+                terminalScore,
+                actionTaken,
+                position);
+
+        ArrayList<StatePython> newChildren = newState2.getChildren();
+        this.children.addAll(newChildren);
+
+        this.expandableMoves.remove(column);
+    }
+
+    public StatePython chooseStrongestChild(double Cp) {
+//        ArrayList<Double> childrenScores = uctScore(...) - ADD LATER
+//        double maxScore = Collections.max(childrenScores);
+//        int bestChildIndex = childrenScores.indexOf(maxScore);
+//        return this.children.get(bestChildIndex);
+        return this.children.get(0);
+    }
+
+    public StatePython choosePlayChild() {
+        ArrayList<Double> childrenScores = new ArrayList<>();
+
+        for (StatePython child : this.children) {
+            childrenScores.add(child.nodeTotalScore);
+        }
+
+        double maxScore = Collections.max(childrenScores);
+        int bestChildIndex = childrenScores.indexOf(maxScore);
+        return this.children.get(bestChildIndex);
+    }
+
+    public Object treeSingleRun() {
+        if (this.isTerminal) {
+//            this.backpropagate(this.terminalScore); ADD
+            return null;
+        }
+//        if (this.isExpandable()) {
+//            this.expandSimulateChild()
+//            return null;
+//        }
+//        this.chooseStrongestChild(Cp_default).treeSingleRun(); where does Cp default come in?
+        return null;
+    }
+
+    public double simulate() {
+        if (this.isTerminal) {
+            return this.terminalScore;
+        }
+//        return - ADD opponentScore function
+        return 0.0;
+    }
+
+    public void backPropagate(double simulationScore) {
+        this.nodeTotalScore += simulationScore;
+        this.nodeTotalVisits += 1;
+        if (!isNull(this.parent)) {
+//            this.parent.backPropagate(*function*); opponentScore
+        }
+    }
+
+    public StatePython chooseChildViaAction(int action) {
+        for (StatePython child : this.children) {
+            if (child.actionTaken == action) {
+                return child;
+            }
+        }
+        return null;
     }
 }
